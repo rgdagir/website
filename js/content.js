00b048fb7a7411fbdc3ca0d34bb436233ddc1cf5
Raul Gallo Dagir <rauldagir@hotmail.com>
2021-08-02 02:21:14 -0400

    Basic design done, working on navbar and onclick features

---
+++ js/content.js
@@ -0,0 +1,14 @@
+var website_content = {
+        "long_bio": `<p>long_bio</p>`,
+        "bio": `<p>bio</p>`,
+        "two_sigma": ``,
+        "hashdex": `
+            <h3><a href="https://www.hashdex.com" contentEditable="false" target="_blank">Hashdex</a></h3>
+            <h4>June to September/2019</h4>
+            <ul>
+                <li>Developed Hashdex’s Order Management System and its exchange integrations from scratch, using C# (.NET). Built the system focusing on quick integration with new exchanges, thus using a hexagonal (“ports and adapters”) architecture.</li>
+                <br></br>
+                <li>Performed a deep analysis of Bitcoin’s on-chain data, using SQL with Goo  gle BigQuery to process over 500 GB of transaction data in order to calculate the network’s realized cap (coinmetrics.io/realized-capitalization) and liveliness, key metrics to understand the market a  nd shift the fund’s strategy to active trading.</li>
+                </ul>`
+}
+
