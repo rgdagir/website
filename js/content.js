ab5a9679256cc939fcbdfcdf96bfb46e3dda0a6d
Raul Gallo Dagir <rauldagir@hotmail.com>
2021-08-03 01:44:27 -0400

    Adding jobs content, improving the jobs navbar design and functionality

--- js/content.js
+++ js/content.js
@@ -1,14 +1,37 @@
 var website_content = {
         "long_bio": `<p>long_bio</p>`,
         "bio": `<p>bio</p>`,
-        "two_sigma": ``,
+        "two_sigma": `
+            <h3><a href="https://www.twosigma.com" contentEditable="false" target="_blank">Two Sigma</a></h3>
+            <h4>Hardware Engineering Intern - July to September/2021</h4>
+            <ul>
+                <li>Working on a novel market data receiver module on an FPGA. Instead of SystemVerilog, this project is being developed using Xilinx's Vitis HLS (high level synthesis) tools and C++.</li>
+            </ul>
+            <h4>Hardware Engineering Intern - June to August/2020</h4>
+            <ul>
+                <li>Built a harness on an FPGA to stress test hardware models under extreme network conditions. The project was built using C++ and SystemVerilog.</li>
+            </ul>`,
         "hashdex": `
             <h3><a href="https://www.hashdex.com" contentEditable="false" target="_blank">Hashdex</a></h3>
-            <h4>June to September/2019</h4>
+            <h4>Software Engineering and Research Intern - June to September/2019</h4>
             <ul>
                 <li>Developed Hashdex’s Order Management System and its exchange integrations from scratch, using C# (.NET). Built the system focusing on quick integration with new exchanges, thus using a hexagonal (“ports and adapters”) architecture.</li>
                 <br></br>
-                <li>Performed a deep analysis of Bitcoin’s on-chain data, using SQL with Goo  gle BigQuery to process over 500 GB of transaction data in order to calculate the network’s realized cap (coinmetrics.io/realized-capitalization) and liveliness, key metrics to understand the market a  nd shift the fund’s strategy to active trading.</li>
-                </ul>`
+                <li>Performed a deep analysis of Bitcoin’s on-chain data, using SQL with Google BigQuery to process over 500 GB of transaction data in order to calculate the network’s realized cap (coinmetrics.io/realized-capitalization) and liveliness, key metrics to understand the market a  nd shift the fund’s strategy to active trading.</li>
+            </ul>`,
+        "facebook":`
+            <h3><a href="https://about.facebook.com/" contentEditable="false" target="_blank">Facebook</a></h3>
+            <h4>Software Engineering Intern (FBU program) - June to August/2018</h4>
+            <ul>
+                <li>Using Java and Node.js, developed a blind dating app, Blind8, for Android mobile devices.</li>
+                <br></br>
+                <li>The project was selected as the best Android app of the FBU (Facebook University for Engineers) internship program, which led to a private presentation of the project to Mark Zuckerberg.</li>
+            </ul>`,
+        "pagarme":`
+            <h3><a href="https://www.pagar.me/" contentEditable="false" target="_blank">Pagar.e</a></h3>
+            <h4>Operations Intern - March to July/2017</h4>
+            <ul>
+                <li>Wrote Python scripts to automate some of the company's internal tasks, such as chargeback conciliation and sending automatic emails for clients in order to verify and activate accounts. Also worked with monitoring the company's operation using SQL, MongoDB and data analysis on the database.</li>
+            </ul>`
 }
 
